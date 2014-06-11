764d41e889d106dd62874f7d1126c4cd7c09c622
Anders Riutta <git@andersriutta.com>
2014-06-10 18:04:08 -0700

    added non-working node example

---
+++ example/node/card.js
@@ -0,0 +1,45 @@
+/* Demo application for Addressbook 
+ */
+// Some From http://brondsema.net/blog/index.php/2006/11/25/javascript_rdfparser_from_tabulator
+
+//var $rdf = require('rdflib');
+var $rdf = require('../../dist/rdflib.js');
+
+// For quick access to those namespaces:
+var FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+var RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+var RDFS = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#');
+var OWL = $rdf.Namespace('http://www.w3.org/2002/07/owl#');
+var DC = $rdf.Namespace('http://purl.org/dc/elements/1.1/');
+var RSS = $rdf.Namespace('http://purl.org/rss/1.0/');
+var XSD = $rdf.Namespace('http://www.w3.org/TR/2004/REC-xmlschema-2-20041028/#dt-');
+var CONTACT = $rdf.Namespace('http://www.w3.org/2000/10/swap/pim/contact#');
+
+
+
+// TestStore implementation from dig.csail.mit.edu/2005/ajar/ajaw/test/rdf/rdfparser.test.html
+// RDFIndexedFormula from dig.csail.mit.edu/2005/ajar/ajaw/rdf/identity.js
+//  (extends RDFFormula from dig.csail.mit.edu/2005/ajar/ajaw/rdf/term.js which has no indexing and smushing)
+// for the real implementation used by Tabulator which uses indexing and smushing
+
+// var kb = new TestStore()
+
+var kb = $rdf.graph();
+
+var uri = 'http://bblfish.net/people/henry/card.rdf';
+
+var person = $rdf.sym(uri);
+
+var docURI;
+if (uri.slice(0, uri.indexOf('#') > -1)) {
+  docURI = uri.slice(0, uri.indexOf('#'));
+} else {
+  docURI = uri;
+}
+
+var fetch = $rdf.fetcher(kb);
+fetch.nowOrWhenFetched(docURI,undefined,function(ok, body){ // @@ check ok
+  console.log(ok);
+  console.log(body);
+});
+
